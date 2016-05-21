42f4b219f0d55967cad21236c883c6dbbf2d1f7a
akherr <akherr@vt.edu>
2016-05-21 18:49:45 -0400

    dont prepare

--- submitContact.php
+++ submitContact.php
@@ -7,8 +7,7 @@ $one = 1;
 $sql = "INSERT INTO `Program_Requests` (`ProgramTypeID`, `GroupTypeID`, `AssignedUserID`, `StatusTypeID`, `FirstName`, `LastName`, `Phone`, `Email`, `Company`, `Role`, `ProposedLocation`, `ProposedDate`, `NumberParticipants`, `EstimatedCost`, `ClientMessage`)"
         . " VALUES($one, $one, $one, $one," . $_POST['first_name'] . "," . $_POST['last_name']. "," . $_POST['phone'] . "," . $_POST['eddress'] . "," . $_POST['organization'] . "," . $_POST['role'] . "," . $_POST['ProgramLocation'] . "," . $_POST['ProgramDate'] . "," . $_POST['NumberParticipants'] . "," . $_POST['EstimatedCost'] . ", 'TEST MESSAGE')";
         
-$result = $conn->prepare($sql);
-$result->execute();
+$result = $conn->query($sql);
 include ($_SERVER['DOCUMENT_ROOT']."showContacts.php");
 //echo "<html><p>$result</p></html>";
 
