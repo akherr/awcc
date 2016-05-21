6b501336361a07763a9528d9c9fb8035217953a0
akherr <akherr@vt.edu>
2016-05-21 17:21:09 -0400

    reformat sql insert

--- submitContact.php
+++ submitContact.php
@@ -2,17 +2,9 @@
 include ($_SERVER['DOCUMENT_ROOT']."dbconnect.php");
 
 //SQL statement for the insert
-$sql = "INSERT INTO `Program_Requests` (`ProgramTypeID`, `GroupTypeID`, `AssignedUserID`, `StatusTypeID`, `FirstName`, `LastName`, `Phone`, `Email`, `Company`, `Role`, `ProposedLocation`, `ProposedDate`, `NumberParticipants`, `EstimatedCost`, `ClientMessage`, `UpdateTime`, `CreatedDate`)"
-        . " VALUES(1, 1, 1, 1, '$_POST[first_name]', '$_POST[last_name]', '$_POST[phone]', '$_POST[edress]', '$_POST[organization]', '$_POST[role]', '$_POST[ProgramLocation]', '$_POST[ProgramDate]', '$_POST[NumberParticipants]', '$_POST[EstimatedCost]', 'TEST MESSAGE', '2016-05-21 16:00:55', '0000-00-00 00:00:00')";
+$sql = "INSERT INTO `Program_Requests` (`ProgramTypeID`, `GroupTypeID`, `AssignedUserID`, `StatusTypeID`, `FirstName`, `LastName`, `Phone`, `Email`, `Company`, `Role`, `ProposedLocation`, `ProposedDate`, `NumberParticipants`, `EstimatedCost`, `ClientMessage`)"
+        . " VALUES(1, 1, 1, 1," . $_POST['first_name'] . "," . $_POST['last_name']. "," . $_POST['phone'] . "," . $_POST['eddress'] . "," . $_POST['organization'] . "," . $_POST['role'] . "," . $_POST['ProgramLocation'] . "," . $_POST['ProgramDate'] . "," . $_POST['NumberParticipants'] . "," . $_POST['EstimatedCost'] . "," . "TEST MESSAGE)";
         
-        
-        
-        
-        //. "values('$_POST[supName]', '$_POST[supAddress]', '$_POST[supCity]',"
-        //. "'$_POST[supState]', '$_POST[supZipcode]', '$_POST[supFax]', '$_POST[supWebsite]',"
-        //. "'$_POST[supEmail]')";
-
-//make the query and store in result
 $result = $conn->query($sql);
 include ($_SERVER['DOCUMENT_ROOT']."showContacts.php");
 echo "<html><p>$result</p></html>";
