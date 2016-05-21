92c01db306a4424e7abf3cd7d98c288751afa386
akherr <akherr@vt.edu>
2016-05-21 17:36:38 -0400

    do we need to specify ID?

--- awcc-last-20160513.sql
+++ awcc-last-20160513.sql
@@ -107,8 +107,8 @@ CREATE TABLE IF NOT EXISTS `Program_Requests` (
 -- Dumping data for table `Program_Requests`
 --
 
-INSERT INTO `Program_Requests` (`ProgramRequestID`, `ProgramTypeID`, `GroupTypeID`, `AssignedUserID`, `StatusTypeID`, `FirstName`, `LastName`, `Phone`, `Email`, `Company`, `Role`, `ProposedLocation`, `ProposedDate`, `NumberParticipants`, `EstimatedCost`, `ClientMessage`, `UpdateTime`, `CreatedDate`) VALUES
-(1, NULL, NULL, NULL, NULL, 'Elvis', 'Presley', '404-777-1234', 'elvis@washere.com', 'The Big Music Machine', 'Lead singer', NULL, NULL, NULL, NULL, 'I''d like an event to kick off my tour next year. I''ve got a new band, new roadies, and my old management team. We need to get acquainted, and we have to come up with some plans and best practices for the tour. Best time: sometime this summer, depending on everyone''s schedules. Could we do this in one or two days?', '2016-05-03 16:00:55', '0000-00-00 00:00:00');
+INSERT INTO `Program_Requests` (`ProgramTypeID`, `GroupTypeID`, `AssignedUserID`, `StatusTypeID`, `FirstName`, `LastName`, `Phone`, `Email`, `Company`, `Role`, `ProposedLocation`, `ProposedDate`, `NumberParticipants`, `EstimatedCost`, `ClientMessage`, `UpdateTime`, `CreatedDate`) VALUES
+(NULL, NULL, NULL, NULL, 'Elvis', 'Presley', '404-777-1234', 'elvis@washere.com', 'The Big Music Machine', 'Lead singer', NULL, NULL, NULL, NULL, 'I''d like an event to kick off my tour next year. I''ve got a new band, new roadies, and my old management team. We need to get acquainted, and we have to come up with some plans and best practices for the tour. Best time: sometime this summer, depending on everyone''s schedules. Could we do this in one or two days?', '2016-05-03 16:00:55', '0000-00-00 00:00:00');
 
 -- --------------------------------------------------------
 
--- submitContact.php
+++ submitContact.php
@@ -2,8 +2,9 @@
 include ($_SERVER['DOCUMENT_ROOT']."dbconnect.php");
 
 //SQL statement for the insert
+
 $sql = "INSERT INTO `Program_Requests` (`ProgramTypeID`, `GroupTypeID`, `AssignedUserID`, `StatusTypeID`, `FirstName`, `LastName`, `Phone`, `Email`, `Company`, `Role`, `ProposedLocation`, `ProposedDate`, `NumberParticipants`, `EstimatedCost`, `ClientMessage`)"
-        . " VALUES(1, 1, 1, 1," . $_POST['first_name'] . "," . $_POST['last_name']. "," . $_POST['phone'] . "," . $_POST['eddress'] . "," . $_POST['organization'] . "," . $_POST['role'] . "," . $_POST['ProgramLocation'] . "," . $_POST['ProgramDate'] . "," . $_POST['NumberParticipants'] . "," . $_POST['EstimatedCost'] . "," . "TEST MESSAGE)";
+        . " VALUES(1, 1, 1, 1," . $_POST['first_name'] . "," . $_POST['last_name']. "," . $_POST['phone'] . "," . $_POST['eddress'] . "," . $_POST['organization'] . "," . $_POST['role'] . "," . $_POST['ProgramLocation'] . "," . $_POST['ProgramDate'] . "," . $_POST['NumberParticipants'] . "," . $_POST['EstimatedCost'] . ", 'TEST MESSAGE')";
         
 $result = $conn->query($sql);
 include ($_SERVER['DOCUMENT_ROOT']."showContacts.php");
