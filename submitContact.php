6b7c0be02298b3246aad559ee5b4850dcd499f5a
akherr <akherr@vt.edu>
2016-05-22 14:29:36 -0400

    wrong variable name

--- submitContact.php
+++ submitContact.php
@@ -5,7 +5,7 @@ include ($_SERVER['DOCUMENT_ROOT']."dbconnect.php");
 
 $one = 1;
 $sql = "INSERT INTO Program_Requests (ProgramTypeID, GroupTypeID, AssignedUserID, StatusTypeID, FirstName, LastName, Phone, Email, Company, Role, ProposedLocation, ProposedDate, NumberParticipants, EstimatedCost, ClientMessage)"
-        . " VALUES($one, $one, $one, $one," . $_POST['first_name'] . "," . $_POST['last_name']. "," . $_POST['phone'] . "," . $_POST['eddress'] . "," . $_POST['organization'] . "," . $_POST['role'] . "," . $_POST['ProgramLocation'] . "," . $_POST['ProgramDate'] . "," . $_POST['NumberParticipants'] . "," . $_POST['EstimatedCost'] . ", 'TEST MESSAGE')";
+        . " VALUES($one, $one, $one, $one," . $_POST['first_name'] . "," . $_POST['last_name']. "," . $_POST['phone'] . "," . $_POST['eddress'] . "," . $_POST['organization'] . "," . $_POST['role'] . "," . $_POST['ProgramLocation'] . "," . $_POST['ProgramDate'] . "," . $_POST['NumberParticipants'] . "," . $_POST['ProgramCost'] . ", 'TEST MESSAGE')";
         
 echo "<html><p>debugging so lets pring sql variable</p></html> ";
 echo "<html><p>$sql</p></html>";
