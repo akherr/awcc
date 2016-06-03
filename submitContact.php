b3b4353b8f095a8ddf3acc847fda3d4979dcc837
akherr <akherr@vt.edu>
2016-06-03 11:25:12 -0400

    move to prepare...testing with submit first

--- submitContact.php
+++ submitContact.php
@@ -1,15 +1,42 @@
 <?php
 include ($_SERVER['DOCUMENT_ROOT']."/dbconnect.php");
 
-//SQL statement for the insert
 
+//variables for prepare statement
 $one = 1;
-$sql = "INSERT INTO Program_Requests (ProgramTypeID, GroupTypeID, AssignedUserID, StatusTypeID, FirstName, LastName, Phone, Email, Company, Role, ProposedLocation, ProposedDate, NumberParticipants, EstimatedCost, ClientMessage)"
-        . " VALUES($one, $one, $one, $one,'" . $_POST['first_name'] . "','" . $_POST['last_name']. "','" . $_POST['phone'] . "','" . $_POST['eddress'] . "','" . $_POST['organization'] . "','" . $_POST['role'] . "','" . $_POST['ProgramLocation'] . "','" . $_POST['ProgramDate'] . "','" . $_POST['NumberParticipants'] . "','" . $_POST['ProgramCost'] . "', 'TEST MESSAGE')";
+$first_name = filter_input(INPUT_POST, 'first_name', FILTER_SANITIZE_STRING);
+$last_name = filter_input(INPUT_POST, 'last_name', FILTER_SANITIZE_STRING);
+$phone = filter_input(INPUT_POST, 'phone', FILTER_SANITIZE_STRING);
+$email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_STRING);
+$company = filter_input(INPUT_POST, 'company', FILTER_SANITIZE_STRING);
+$role = filter_input(INPUT_POST, 'role', FILTER_SANITIZE_STRING);
+$proposed_location = filter_input(INPUT_POST, 'proposed_location', FILTER_SANITIZE_STRING);
+$proposed_date = filter_input(INPUT_POST, 'proposed_date', FILTER_SANITIZE_STRING);
+$number_participants = filter_input(INPUT_POST, 'number_participants', FILTER_SANITIZE_STRING);
+$estimated_cost = filter_input(INPUT_POST, 'estimated_cost', FILTER_SANITIZE_STRING);
+$client_message = filter_input(INPUT_POST, 'client_message', FILTER_SANITIZE_STRING);
+
+//SQL statement for the insert
+//$sql = "INSERT INTO Program_Requests (program_type_id, group_type_id, assigned_user_id, status_type_id, first_name, last_name, phone, email, company, role, proposed_location, proposed_date, number_participants, estimated_cost, client_message)"
+        //. " VALUES($one, $one, $one, $one,'" . $_POST['first_name'] . "','" . $_POST['last_name']. "','" . $_POST['phone'] . "','" . $_POST['eddress'] . "','" . $_POST['organization'] . "','" . $_POST['role'] . "','" . $_POST['ProgramLocation'] . "','" . $_POST['ProgramDate'] . "','" . $_POST['NumberParticipants'] . "','" . $_POST['ProgramCost'] . "', 'TEST MESSAGE')";
         
-echo "<html><p>debugging so lets pring sql variable</p></html> ";
+
+if ($stmt = $conn->prepare("INSERT INTO  (program_type_id, group_type_id, assigned_user_id, status_type_id, first_name, last_name, phone, email, company, role, proposed_location, proposed_date, number_participants, estimated_cost, client_message) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ? )")) {
+ 
+    // Bind the variables to the parameter as strings. 
+    $stmt->bind_param("iiiisssssssssss", $one, $one, $one, $one, $first_name, $last_name, $phone, $email, $company, $role, $proposed_location, $proposed_date, $number_participants, $estimated_cost, $client_message);
+ 
+    // Execute the statement.
+    $stmt->execute();
+ 
+    // Close the prepared statement.
+    $stmt->close();
+ 
+}
+
+echo "<html><p>debugging so lets print sql variable</p></html> ";
 echo "<html><p>$sql</p></html>";
-$result = $conn->query($sql);
+//$result = $conn->query($sql);
 include ($_SERVER['DOCUMENT_ROOT']."/showContacts.php");
 echo "<html><p>$result</p></html>";
 
