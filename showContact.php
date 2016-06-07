7e01615bce80293bf9599afc6cdfc9ffc835f4a8
akherr <akherr@vt.edu>
2016-06-07 15:10:56 -0400

    add the table wrapper

--- showContact.php
+++ showContact.php
@@ -15,6 +15,20 @@ if ($stmt = $conn->prepare("SELECT * FROM Program_Requests WHERE program_request
     $result = $stmt->get_result();
  
     $row = $result->fetch_assoc();
+    
+    $numfields = $conn->field_count;
+    $numrows = $result->num_rows;
+
+    echo '<html><p> Number of Fields: ' . $numfields . ' Number of rows: ' . $numrows . '</p>';
+    echo "<table border='1' id='resultstable'><tr>";
+    echo "<td id='tableheaders'></td>";
+
+    for($i=0; $i<$numfields; $i++)
+    {
+        $fieldinfo = mysqli_fetch_field_direct($results, $i);
+        echo "<td id='tableheaders'>" . $fieldinfo->name . "</td>";
+    }
+    
     echo '<tr><td>' . $row['program_request_id'] . '</td>';
     echo '<td>' . $row['program_type_id'] . '</td>';
     echo '<td>' . $row['group_type_id'] . '</td>';
@@ -33,7 +47,7 @@ if ($stmt = $conn->prepare("SELECT * FROM Program_Requests WHERE program_request
     echo '<td>' . $row['client_message'] . '</td>';
     echo '<td>' . $row['updated'] . '</td>';
     echo '<td>' . $row['created'] . '</td></tr>';
-    
+    echo '</table></html>';
     // Close the prepared statement.
     $stmt->close();
  
